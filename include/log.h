4809e0126f1562e7edc86a632195a33a51744fe1
Edward Sargsyan <edward.sarkisyan@gmail.com>
2020-06-30 12:37:26 +0200

    Cleanup

--- include/log.h
+++ include/log.h
@@ -6,9 +6,24 @@
 //#define LOG_FMT " In:\n" << __FILE__ << ":" << __LINE__ << " " << F_NAME << ":\n\t"
 #define LOG_FMT " " << F_NAME << ":"
 
+#ifndef LOG_DEBUG
 #define LOG_DEBUG std::cout << "\nDBG" << LOG_FMT
+#endif
+
+#ifndef LOG_INFO
 #define LOG_INFO  std::cout << "\nINF" << LOG_FMT
+#endif
+
+#ifndef LOG_ERROR
 #define LOG_ERROR std::cerr << "\nERR" << LOG_FMT
+#endif
+
+#ifndef LOG_WARN
 #define LOG_WARN  std::cout << "\nWRN" << LOG_FMT
+#endif
+
+#ifndef LOG_WARNING
 #define LOG_WARNING LOG_WARN
+#endif
+
 #endif // LOG_H
